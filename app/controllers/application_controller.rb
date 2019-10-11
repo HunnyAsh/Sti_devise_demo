25bfe8f0564e19188b87c53e37c9fe3b0670a710
HunnyAsh <jummanihunny@gmail.com>
2019-10-11 08:51:51 +0530

    Chnages in root path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   private
 
   def after_sign_in_path_for(resource)
+    binding.pry
     resource.type.eql?('Admin') ? dashboard_path : root_path
   end
 end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,4 @@
 class DashboardController < ApplicationController
-  before_action :authenticate_admin!
   def index
     @admins = Admin.all
     @users = User.all
